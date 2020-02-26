466e25fcd9f26ae0df73f54df2f529a23c7b7aa2
OscarDelToro <oscar.deltoro@icloud.com>
2020-02-26 02:37:56 +0000

    Se agregó el feature de apagado

---
+++ xv6-public/apagar.c
@@ -0,0 +1,14 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+char buf[512];
+
+
+int
+main(int argc, char *argv[])
+{
+  printf(1,"Hasta la vista...\n");
+  shutdown();
+  exit();
+}
